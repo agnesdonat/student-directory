15d5b6a441667bb9812e8aa4a8458034d0d4bd23
Agnes Donat <agi.donat@gmail.com>
2018-02-15 16:08:30 +0000

    Exercise 4

--- directory.rb
+++ directory.rb
@@ -23,13 +23,11 @@ def print_header
 end
 
 def print(students) 
-    
-    students.each do |student|
-      if student[:name].length < 12
-        puts "#{student[:name]} #{student[:cohort]}"
-      end
-    end
-  
+  i = 0    
+  while students.length > i do
+    puts students[i][:name]
+    i += 1
+  end
 end
 
 def print_footer(names) 
