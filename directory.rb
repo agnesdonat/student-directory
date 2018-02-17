5395a4c2507e4c2ef5ed962a3a7da7a5da92fdef
Agnes Donat <agi.donat@gmail.com>
2018-02-17 16:47:56 +0000

    More refactoring

--- directory.rb
+++ directory.rb
@@ -1,8 +1,10 @@
+@students = [] # an empty array accessible to all methods
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
     # create an empty array
-  students = []
+    # students = []
     # get the first name
   name = gets.strip
     # store information in variable "info" from more_info method
@@ -12,11 +14,11 @@ def input_students
     # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: info[:cohort], hobby: info[:hobby], country: info[:country], height: info[:height]}
-    if students.length == 1
+    @students << {name: name, cohort: info[:cohort], hobby: info[:hobby], country: info[:country], height: info[:height]}
+    if @students.length == 1
       puts "Now we have 1 student."
     else
-      puts "Now we have #{students.count} students."
+      puts "Now we have #{@students.count} students."
     end  
     # get another name from the user
     name = gets.strip
@@ -25,7 +27,7 @@ def input_students
     end  
   end
     # return the array of students
-    students
+    @students
 end
 
 def more_info
@@ -46,35 +48,65 @@ def more_info
   puts "Please enter their height"
   height = gets.chop.to_i
   return {cohort: cohort, hobby: hobby, country: country, height: height}
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
 end  
+
+def print_menu
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" # 9 because we'll be adding more items
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again."
+  end  
+end
 
 def print_header
  puts "The students of my cohort at Makers Academy
   -------------".lines.map {|line| line.strip.center(120) }
 end
 
-def print(students)
+def print_student_list
   i = 0
-  
-  while students.length > i do
-  puts "name: #{students[i][:name]}, cohort: #{students[i][:cohort]}, hobby: #{students[i][:hobby]}, country: #{students[i][:country]}, height: #{students[i][:height]} cm".center(120)
+  while @students.length > i do
+  puts "name: #{@students[i][:name]}, cohort: #{@students[i][:cohort]}, hobby: #{@students[i][:hobby]}, country: #{@students[i][:country]}, height: #{@students[i][:height]} cm".center(120)
   i += 1
   end
 end
 
-def print_footer(students)
-  
-  if students.length == 0
+def print_footer
+  if @students.length == 0
     puts "\n We have no students enrolled at the moment.".lines.map {|line| line.strip.center(120)}
-  elsif students.length == 1
+  elsif @students.length == 1
     puts "\n Overall, we have 1 great student.".lines.map {|line| line.strip.center(120)}
   else
-    puts "\n Overall, we have #{students.count} great students.".lines.map {|line| line.strip.center(120)}
+    puts "\n Overall, we have #{@students.count} great students.".lines.map {|line| line.strip.center(120)}
   end
-  
 end  
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+interactive_menu 
+# @students = input_students
+# print_header
+# print(students)
+# print_footer(students)
